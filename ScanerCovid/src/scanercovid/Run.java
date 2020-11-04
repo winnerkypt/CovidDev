98289b5a19bedd57b90bc1dc8ec84fa00d123bc5
salmonindaeyo <iamfhaa@hotmail.com>
2020-11-04 21:42:21 +0700

    แก้ไขครั้งที่ 3

--- ScanerCovid/src/scanercovid/Run.java
+++ ScanerCovid/src/scanercovid/Run.java
@@ -18,5 +18,6 @@ public class Run {
         //run
         
         a.menu();
+        
     }
 }
--- ScanerCovid/src/scanercovid/ScanerCovid.java
+++ ScanerCovid/src/scanercovid/ScanerCovid.java
@@ -17,6 +17,7 @@ public class ScanerCovid {  ////======================ตัวแปร========
     String namePerson;    ////ชื่อคนที่เข้าร้าน
     String [] person;     ////ชื่อของคนที่กำลังอยู่ในร้านตอนนี้
     String [] personCopy; /////รายชื่อคนทั้งหมดที่เข้าร้าน
+    String [] phone;       ////เบอร์ 
     
     int positionOfperson = 0;  ////ตำแหน่งของคนที่เข้าร้านใหม่ 
     int n = 0; /////ตำแหน่งอันเลย์รายชื่อคนทั้งหมด เอาไว้แอดรายชื่อคนเข้าเรื่อยๆ
@@ -40,12 +41,27 @@ public class ScanerCovid {  ////======================ตัวแปร========
         Scanner scn=new Scanner(System.in);
         int menu;
         
-        System.out.println("ยินดีต้อนรับเข้าสู่ "+nameOfplace);
-        System.out.println("กด 1 เพื่อลงชื่อเข้าร้าน");
-        System.out.println("กด 2 เพื่อลงชื่อออกร้าน");
-        System.out.println("กด 3 เพื่อเช็คคนทั้งหมดที่เข้าร้าน");
-        System.out.println("===จำนวนคนที่สามารถอยู๋ในร้านได้คือ "+person.length+"===");
-        System.out.println("===จำนวนคนในร้านตอนนี้ "+numberNow+"===");
+//        StringBuilder welcome = new StringBuilder();
+//        welcome.append("\nยินดีต้อนรับเข้าสู่ \nกด 1 เพื่อลงชื่อเข้าร้าน");
+//        System.out.println(welcome.toString());
+////        welcome.append("\nกด 1 เพื่อลงชื่อเข้าร้าน");
+//        welcome.append("\nกด 2 เพื่อลงชื่อออกร้าน");
+//        System.out.println(welcome.toString());
+//        welcome.append("\nกด 3 เพื่อเช็คคนทั้งหมดที่เข้าร้าน");
+//        System.out.println(welcome.toString());
+//        welcome.append("\n===จำนวนคนที่สามารถอยู๋ในร้านได้คือ "+person.length+"===");
+//        System.out.println(welcome.toString());
+//        welcome.append("\n===จำนวนคนในร้านตอนนี้ "+numberNow+"===");
+//        System.out.println(welcome.toString());
+        
+        System.out.print("ยินดีต้อนรับเข้าสู่ "+nameOfplace +
+                "\n กด 1 เพื่อลงชื่อเข้าร้าน"+
+                "\n กด 2 เพื่อลงชื่อออกร้าน"+
+                "\n กด 3 เพื่อเช็คคนทั้งหมดที่เข้าร้าน"+
+                "\n ===จำนวนคนที่สามารถอยู๋ในร้านได้คือ "+person.length+"==="+
+                "\n ===จำนวนคนในร้านตอนนี้ "+numberNow+"==="
+                );
+           
         menu=scn.nextInt();
         
         
@@ -73,20 +89,47 @@ public class ScanerCovid {  ////======================ตัวแปร========
         Scanner scn=new Scanner(System.in);
         
         int LoopOfJoin = 0;
-        
+        double tem = 0;
         
         ////===================วนลูปเพิ่มคนเรื่อยๆจนกว่าจะกดยกเลิก============/////////////
         while(LoopOfJoin==0){
+            
+            
+            StringBuilder temp = new StringBuilder();
+            temp.append("อุณหภูมิของร่างกาย");
+            tem=scn.nextDouble();
+            if(tem>=37.5){
+                
+                temp.append("======================");
+                temp.append("===อุณหภูมิเกินห้ามเข้า===");
+                temp.append("======================");
+                temp.append("===กรุณาใส่รายชื่อต่อไป===");
+                System.out.println(temp.toString());
+                personJoin();
+                
+                
+                
+            }
+            
+            
+            System.out.println("====================");
           System.out.println("คนในร้านทั้งหมดตอนนี้"+Arrays.toString(person));
             System.out.println("ตำแหน่งในร้าน");
             
         positionOfperson=scn.nextInt();  ////////  <<<<<<<<<--------- 
         
-            System.out.println("ชื่อคนที่เข้าร้าน");
+            System.out.println("ใส่ชื่อคนที่เข้าร้าน");
         namePerson=scn.next();
         person[positionOfperson]=namePerson;
         allPersonWhoJoinMyPlace();
         numberNow+=1;
+        
+        
+            System.out.println("ใส่เบอร์โทร");
+            phone[positionOfperson]=scn.next();
+                
+        
+        
         
         
         
